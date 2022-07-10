5719ae2308f542e2764cffbd9ba02b4da2dd52e8
Pavel Osminin <osperevod@gmail.com>
2022-07-10 21:47:27 +0500

    Shape interface is implemented for triangle.go

--- triangle.go
+++ triangle.go
@@ -1,6 +1,20 @@
 package golang_united_school_homework
 
+import (
+	"math"
+)
+
 // Triangle must satisfy to Shape interface
 type Triangle struct {
 	Side float64
+}
+
+func (t Triangle) CalcPerimeter() float64 {
+	// assume the trianlge is equilateral
+	return 3 * t.Side
+}
+
+func (t Triangle) CalcArea() float64 {
+	// assume the trianlge is equilateral
+	return (math.Sqrt(3) * t.Side * t.Side) / 4
 }
